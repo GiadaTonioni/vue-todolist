286dbbbb04e0e1e7e5a265a579bedc470a603e54
GiadaTonioni <giada.tonioni@gmail.com>
2023-06-11 14:39:50 +0200

    inserimento array di oggetti e true-false

--- js/script.js
+++ js/script.js
@@ -5,6 +5,35 @@ let id = 0
 createApp({
   data() {
     return {
+      tasks: [
+        {
+          text: 'Fare la spesa',
+          done: false
+        },
+        {
+          text: 'Chiamare la doc',
+          done: true
+        },
+        {
+          text: 'Pagare le bollette',
+          done: false
+        },
+        {
+          text: 'Pulire casa',
+          done: true
+        },
+        {
+          text: 'Fare un backup',
+          done: true
+        },
+        {
+          text: 'Fare il pieno alla macchina',
+          done: false
+        },
+      ]
     }
   },
+  methods:{
+
+  }
 }).mount('#app')
--- readme.md
+++ readme.md
@@ -2,4 +2,8 @@ PROBLEMA: Fare una todolist, Creare un array di oggetti ( con proprietà text e 
 
 1-creare la versione statica della todolist
     1.1 - creare una ul
-2- 
+2- Creo un array di oggetti con le proprietà text e done
+3 - Richiamo la proprietà text di ogni oggetto nel dom con v-for
+4 - con binding creo una classe task done andando a richiamare il true o false 
+5 - Creo un modo per cambiare stato ad un task
+
